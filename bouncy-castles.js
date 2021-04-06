eed7d7d8e54a4cb92f2087f7f771afb8da062a78
Neerav Patel <nickpatel.developer@gmail.com>
2021-04-06 00:07:29 -0400

    Kata 11 assignment, Bouncy Castles

---
+++ bouncy-castles.js
@@ -0,0 +1,62 @@
+// Use the value below whenever you need the value of Pi
+const PI = 3.14159;
+
+const sphereVolume = function (radius) {
+  // Code here!
+  return (4 / 3) * PI * (radius * radius * radius);
+};
+
+console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
+
+const coneVolume = function (radius, height) {
+  // And here!
+  return (1 / 3) * PI * (radius * radius) * height;
+};
+
+console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
+
+const prismVolume = function (height, width, depth) {
+  // Probably here too!
+  return width * depth * height;
+};
+
+console.log(prismVolume(3, 4, 5) === 60);
+
+const totalVolume = function (solids) {
+  // Code here? Yup!
+  let vol = 0;
+  solids.forEach((element) => {
+    switch (element['type']) {
+      case 'sphere':
+        vol = vol + sphereVolume(element['radius']);
+        break;
+      case 'cone':
+        vol = vol + coneVolume(element['radius'], element['height']);
+        break;
+      case 'prism':
+        vol = vol + prismVolume(element['height'], element['width'], element['depth']);
+        break;
+    }
+  });
+  return vol;
+};
+
+const largeSphere = {
+  type: 'sphere',
+  radius: 40,
+};
+
+const smallSphere = {
+  type: 'sphere',
+  radius: 10,
+};
+
+const cone = {
+  type: 'cone',
+  radius: 3,
+  height: 5,
+};
+
+const duck = [largeSphere, smallSphere, cone];
+
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
